eebf90ee34d89bf0f793ca94a0a7ec2c6de22ceb
NumeriTTos <124afil@gmail.com>
2026-01-30 23:24:58 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 // ------------------------------
-// 1. Importar Firebase
+// 1. Importar Firebase (versão modular v9+)
 // ------------------------------
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
@@ -9,12 +9,6 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-s
 // ------------------------------
 // 2. Configuração do Firebase
 // ------------------------------
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAyoIj0xb9Vk-kz0Ln8pwIx7rUcrp_F6aM",
   authDomain: "wapp-1f195.firebaseapp.com",
@@ -23,9 +17,6 @@ const firebaseConfig = {
   messagingSenderId: "1042673353512",
   appId: "1:1042673353512:web:0a01dd53b9af3fa70b061d"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 // ------------------------------
 // 3. Inicializar Firebase
@@ -38,5 +29,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-
